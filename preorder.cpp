53ac9dac9cfef58cd8fad148d656934c0cdd6669
supriyabalakrishna <supriyabalakrishna598@gmail.com>
2025-11-28 19:17:48 +0530

    Add files via upload

---
+++ preorder.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+    struct node *left;
+    ;
+    struct node *right;
+};
+struct node *newnode(int val)
+{
+    struct node *temp = new node();
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+void preorder(struct node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data;
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main()
+{
+    struct node *root = newnode(1);
+    root->left = newnode(2);
+    root->left->left = newnode(4);
+    root->left->right = newnode(5);
+    root->left->right->left = newnode(6);
+    root->right = newnode(3);
+    root->right->left = newnode(7);
+    root->right->right = newnode(8);
+    root->right->right->left = newnode(9);
+    root->right->right->right = newnode(10);
+    preorder(root);
+    cout << endl;
+    return 0;
+}
